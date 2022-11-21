cab1e43951518b8c1ac6a00d0bb8b027fb24ce65
Chioma Nwocha <chiomanwocha@Chiomas-MacBook-Pro.local>
2022-11-21 09:42:29 +0100

    implemented external css

--- src/views/LoginTodo.vue
+++ src/views/LoginTodo.vue
@@ -47,69 +47,10 @@ import router from '@/router'
                     this.error = true
                     this.errorMessage = 'this email does not exist !'
                 } 
-                // console.log(this.storedEmail)
-                
             },
         }
     }
 </script>
 
-<style scoped>
-    .container{
-        width: 40vw;
-        margin: 0 auto;
-        padding: 30px 0;
-    }
-    .container > p{
-        color: red
-    }
-    .welcome-hero{
-        font-size: 32px;
-        margin-bottom: 55px
-    }
-    .welcome-hero > p {
-        margin: 0
-    }
-    div > p:first-child{
-        font-weight: 900;
-    }
-    input{
-        width: 25vw;
-        border-radius: 9px;
-        outline: none;
-        padding: 10px;
-        border: 1px solid;
-        margin: 20px;
-        font-size: 16px;
-        background-color: #C2BAFD;
-        color: white;
-        font-weight: 900;
-    }
-    ::placeholder{
-        color: white
-    }
-    button{
-        display: block;
-        margin: 0 auto;
-        width: 26vw;
-        font-size: 16px;
-        background-color: white;
-        border: none;
-        cursor: pointer;
-        padding: 10px 15px;
-        border-radius: 4px;
-        font-weight: 900;
-        box-shadow: rgb(173 173 173) 0px 1px 10px;
-    }
-    form > p {
-        margin-top: 50px
-    }
-    a{
-        font-weight: 900;
-        color: white
-    }
-    button:hover{
-      background-color: hsl(0, 0%, 95%);
-      transition: .1s;
-    }
+<style scoped src="@/assets/css/login-todo.css">
 </style>
